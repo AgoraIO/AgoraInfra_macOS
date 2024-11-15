c4bc7b2581e9008c3cba9f5b0fdce19e819fc0b9
agorabuilder <agorabuilder@agora.io>
2024-11-15 04:36:33 +0000

    0.0.17-test

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "AgoraInfra_macOS",
+    defaultLocalization: "en",
+    platforms: [.macOS(.v10_10)],
+    products: [
+        .library(name: "AgoraInfra_macOS", targets: ["aosl"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "aosl",
+            url: "https://download.agora.io/swiftpm/AgoraInfra_macOS/0.0.17-test/aosl.xcframework.zip",
+            checksum: "517ab87bbc288a3ed71670d1776fbac09d20b1dfeb984680e15e7483c32b272a"
+        ),
+    ]
+)
